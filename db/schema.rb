b16ffd0e49c2fe181e0225b6863d882fa6831077
Benoit Paternot <benoit.paternot@gmail.com>
2017-06-16 16:03:18 +0200

    with bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616134509) do
+ActiveRecord::Schema.define(version: 20170616140150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date     "date"
+    t.date     "start_time"
+    t.string   "player_optional"
+    t.integer  "user_id"
+    t.integer  "tennis_court_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["tennis_court_id"], name: "index_bookings_on_tennis_court_id", using: :btree
+    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
+  end
 
   create_table "clubs", force: :cascade do |t|
     t.string   "name_club"
@@ -57,5 +69,7 @@ ActiveRecord::Schema.define(version: 20170616134509) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "bookings", "tennis_courts"
+  add_foreign_key "bookings", "users"
   add_foreign_key "tennis_courts", "clubs"
 end
